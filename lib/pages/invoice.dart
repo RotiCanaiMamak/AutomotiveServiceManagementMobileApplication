01e3c8f7113370271508a442ead7adcf676abbe0
qianfu <qianfu1016@gmail.com>
2025-08-20 01:30:30 +0800

    v1

---
+++ lib/pages/invoice.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Invoicepage extends StatelessWidget {
+  const Invoicepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text("Invoice Page"),
+      ),
+    );
+  }
+}
---
+++ lib/pages/vehicle.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Vehiclepage extends StatelessWidget {
+  const Vehiclepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text("Vehicle Page"),
+      ),
+    );
+  }
+}
