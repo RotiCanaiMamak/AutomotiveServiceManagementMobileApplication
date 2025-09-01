b6cd5c5ca23b5ab9af8ed5d47d288089344f91d0
qianfu <qianfu1016@gmail.com>
2025-09-01 23:10:10 +0800

    v2

--- lib/pages/add vehicle.dart
+++ lib/pages/add vehicle.dart
@@ -41,7 +41,98 @@ class _AddVehiclepageState extends State<AddVehiclepage> {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Car Loan Calculation"),
+      ),
+      body: Form(
+        key: _formkey,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Loan Amount',
+                ),
+                keyboardType: TextInputType.number,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                controller: lactrl,
+                validator: (value){
+                  if(value == null || value.isEmpty){
+                    return "Please Enter Loan Amount!";
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Down Payment Amount',
+                ),
+                keyboardType: TextInputType.number,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                controller: dpactrl,
+                validator: (value){
+                  if(value == null || value.isEmpty){
+                    return "Please Enter Down Payment Amount!";
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: true,
+                  signed: false
+              ),
+                inputFormatters: [FilteringTextInputFormatter.allow(RegExp(r'[0-9.]'))],
+                decoration: const InputDecoration(
+                  labelText: 'Interest rate (%)',
+                ),
+
+                controller: iratectrl,
+                validator: (value){
+                  if(value == null || value.isEmpty){
+                    return "Please Enter Interest rate!";
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Loan Period (years)',
+                ),
+                keyboardType: TextInputType.number,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                controller: periodctrl,
+                validator: (value){
+                  if(value == null || value.isEmpty){
+                    return "Please Enter Loan Period!";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              Text(_replaymentoutput),
+              SizedBox(height: 20),
+              ElevatedButton(onPressed : (){
+                if(_formkey.currentState!.validate()){
+                  calculateloan();
+                }
+              }, child: const Text("Calculate"))
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  @override
+  void dispose(){
+    super.dispose();
+    lactrl.dispose();
+    dpactrl.dispose();
+    iratectrl.dispose();
+    periodctrl.dispose();
   }
 }
 
